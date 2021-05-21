351d21c72cc414503f885784aa54a494191380d5
Adriana <am20dipi@alum.siena.edu>
2021-05-21 17:33:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31 
+let num2 = 2
+let num3 = 52
+let num4 = 6
+
+let multiply = (num1 * num2)
+
+let mod = (num3 % num4)
+
+let max = Math.max(1, 2, 10, 12, 20);
+
+let random = Math.floor( Math.random() * 1000 ) + 1
